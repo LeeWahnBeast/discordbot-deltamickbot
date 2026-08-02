25cd992ee9e374122f1c3e457f8fd7ed32f94f9a
LeeWahnBeast <phucwesdz145@gmail.com>
2026-08-02 11:15:12 +0700

    Create conga.py

---
+++ conga.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+class DmSpamCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="dmspam")
+    async def dmspam(self, ctx, count: int = 100):
+        user_id = 1314617091139305584
+        user = await self.bot.fetch_user(user_id)
+        msg = "con chó vào code hộ tao🤣🤣🤣🤣 gà kid con toàn đi kiếm chuyện kẻ yếu mà đòi mạnh nhất ss🤣🤣🤣🤣🤣🤣🤣"
+        for _ in range(count):
+            await user.send(msg)
+            await asyncio.sleep(0.2)
+        await ctx.send("done")
+
+async def setup(bot):
+    await bot.add_cog(DmSpamCog(bot))
