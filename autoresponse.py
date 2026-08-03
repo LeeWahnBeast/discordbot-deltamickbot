11a752862e7d60b6432f194954d21f5670f7fa1c
LeeWahnBeast <phucwesdz145@gmail.com>
2026-08-03 19:12:36 +0700

    Update autoresponse.py

--- autoresponse.py
+++ autoresponse.py
@@ -1,17 +1,26 @@
 import discord
+import re
+import unicodedata
+from unidecode import unidecode
 from responses import responses
+
+def normalize(text):
+    text = unicodedata.normalize("NFKC", text)
+    text = unidecode(text).lower()
+    text = re.sub(r"[^a-z0-9]", "", text)
+    return text
 
 async def check(message):
     if message.author.bot:
         return
 
-    msg = message.content.lower()
+    msg = normalize(message.content)
 
     for trigger, data in responses.items():
-        if trigger in msg:
+        if normalize(trigger) in msg:
             if data["image"]:
                 await message.channel.send(
-                    data["text"],
+                    content=data["text"],
                     file=discord.File(data["image"])
                 )
             else:
