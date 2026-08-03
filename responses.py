4fd79d8f471b2fc48c1e95e2f0b8070d23ac003c
LeeWahnBeast <phucwesdz145@gmail.com>
2026-08-03 19:27:33 +0700

    Fix response structure and add missing entry

--- responses.py
+++ responses.py
@@ -6,5 +6,9 @@ responses = {
     "<@1210771747889090571>": {
         "text": None,
         "image": "images/Screenshot_20260803-191353_Discord.png"
-        }
+    },
+    "<@!1210771747889090571>": {
+        "text": None,
+        "image": "images/Screenshot_20260803-191353_Discord.png"
+    }
 }
