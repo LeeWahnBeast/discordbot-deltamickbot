ccefbb1ed4f41df725e9136a4b11fdbbda8a97fa
LeeWahnBeast <phucwesdz145@gmail.com>
2026-08-03 18:41:29 +0700

    Update autoresponse.py

--- autoresponse.py
+++ autoresponse.py
@@ -1,28 +1,19 @@
 import discord
-from discord.ext import commands
 from autoresponse import responses
 
-class AutoResponse(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+async def check(message):
+    if message.author.bot:
+        return
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author.bot:
-            return
+    msg = message.content.lower()
 
-        msg = message.content.lower()
-
-        for trigger, data in responses.items():
-            if trigger in msg:
-                if data["image"]:
-                    await message.channel.send(
-                        data["text"],
-                        file=discord.File(data["image"])
-                    )
-                else:
-                    await message.channel.send(data["text"])
-                break
-
-async def setup(bot):
-    await bot.add_cog(AutoResponse(bot))
+    for trigger, data in responses.items():
+        if trigger in msg:
+            if data["image"]:
+                await message.channel.send(
+                    data["text"],
+                    file=discord.File(data["image"])
+                )
+            else:
+                await message.channel.send(data["text"])
+            break
