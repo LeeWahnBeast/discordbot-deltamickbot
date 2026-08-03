16eaf6ea77483995d7400fc3b714fffd76cdcc6b
LeeWahnBeast <phucwesdz145@gmail.com>
2026-08-03 19:26:21 +0700

    Update image paths in responses.py

--- responses.py
+++ responses.py
@@ -2,9 +2,9 @@ responses = {
     "ai hỏi": {
         "text": None,
         "image": "images/images_55.jpg"
-    }
+    },
     "<@1210771747889090571>": {
         "text": None,
-        "image": "images/"
+        "image": "images/Screenshot_20260803-191353_Discord.png"
         }
 }
