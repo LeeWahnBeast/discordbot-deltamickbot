d504e4841bfd22109ec7cabc4bdc8a17a4ecbea7
LeeWahnBeast <phucwesdz145@gmail.com>
2026-08-08 19:24:39 +0700

    Update responses.py

--- responses.py
+++ responses.py
@@ -14,5 +14,29 @@ responses = {
     "larp": {
         "text": "Hành tây hành tây 🤣🤣🤣",
         "image": None
+    },
+    "verity": {
+        "text": "nhìn xuống bạn* Xin chào, mình là Verity…người bạn trợ lý cá nhân của bạn! Cứ hỏi mình bất cứ điều gì… mình biết tất cả mọi thứ~ *cười khẽ và nhếch mép*",
+        "image": "images/Screenshot_20260808-191331_TikTok.png"
+    },
+    "ratio": {
+        "text": "ratio kid",
+        "image": None
+    },
+    "cơm": {
+        "text": "cơm quê dượng bầu or cơm quê 1989",
+        "image": None
+    },
+    "quê dượng bầu": {
+        "text": "giá trên mây😂😂😂",
+        "image": None
+    },
+    "quê 1989": {
+        "text": "ăn ngon ko",
+        "image": None
+    },
+    "quán nhậu 5 thùy": {
+        "text": "quán nhậu RATIO",
+        "image": None
     }
 }
