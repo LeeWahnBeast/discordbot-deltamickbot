e16afc9372388e156885f421c595b0ac9014c08f
LeeWahnBeast <phucwesdz145@gmail.com>
2026-08-08 19:32:34 +0700

    Update autoresponse.py

--- autoresponse.py
+++ autoresponse.py
@@ -4,11 +4,13 @@ import unicodedata
 from unidecode import unidecode
 from responses import responses
 
+
 def normalize(text):
     text = unicodedata.normalize("NFKC", text)
     text = unidecode(text).lower()
     text = re.sub(r"[^a-z0-9]", "", text)
     return text
+
 
 async def check(message):
     if message.author.bot:
@@ -18,11 +20,26 @@ async def check(message):
 
     for trigger, data in responses.items():
         if normalize(trigger) in msg:
+
+            # Có ảnh
             if data["image"]:
-                await message.channel.send(
-                    content=data["text"],
-                    file=discord.File(data["image"])
-                )
-            else:
+                file = discord.File(data["image"])
+
+                # Có cả text và ảnh
+                if data["text"]:
+                    await message.channel.send(
+                        content=data["text"],
+                        file=file
+                    )
+
+                # Chỉ có ảnh
+                else:
+                    await message.channel.send(
+                        file=file
+                    )
+
+            # Chỉ có text
+            elif data["text"]:
                 await message.channel.send(data["text"])
+
             break
